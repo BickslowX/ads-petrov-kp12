7afafb5d4c3e3b223dd7cd495437fbe749b593b3
Leonid <leo.petrov.2004@gmail.com>
2021-10-20 17:49:15 +0300

    lab1 done

---
+++ lab1/task1/ASD1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Math;
+namespace ASD1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input x"); double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Input y"); double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Input z"); double z = Convert.ToDouble(Console.ReadLine());
+            double a, b;
+            if (Sin(PI + x) != 0 && z != 0 && y != 0)
+            {
+                a = (1 / (2 * Sin(PI + x))) + Pow(Sin((x + y) / z), 2);
+                b = (Cos(Pow(a, 2) * x)) / 2 * y * z;
+                Console.WriteLine($"a = {a}\nb = {b}");
+            }
+            else
+            {
+                Console.WriteLine("Error, input another x, y, z");
+            }
+        }
+    }
+}
